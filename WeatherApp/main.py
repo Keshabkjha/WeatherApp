16d3d83a2e8e6a9776ff14514058ce953c626384
Keshab Kumar <keshabkumarjha876@gmail.com>
2024-10-24 02:43:15 +0530

    COMMIT

--- WeatherApp/main.py
+++
@@ -1,5 +0,0 @@
-from app.weather_interface import WeatherApp
-
-if __name__ == "__main__":
-    app = WeatherApp()
-    app.main_window()
---
+++ weather/backend.py
@@ -0,0 +1,16 @@
+import requests
+
+API_KEY = "ba49b0ebd03dd496854f2123d3bf0407"
+
+
+def get_data(place, forecast_days=None):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+
